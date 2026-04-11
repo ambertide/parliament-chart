35a561a5577c9396f1d862c7e629c1c560631eae
Ege Özkan <egeemirozkan24@gmail.com>
2026-04-12 01:00:30 +0300

    chore: add auto-redirect to terms -> milestones

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   redirects: async () => ([
     {
-      source: '/',
-      destination: '/terms/28',
-      permanent: true
+      source: '/:lang',
+      destination: '/:lang/terms/28',
+      permanent: false
+    },
+    {
+      source: "/:lang/terms/:term",
+      destination: "/:lang/terms/:term/formation",
+      permanent: false
     }
   ])
   /* config options here */
