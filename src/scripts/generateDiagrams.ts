b94b6d266b1a9584e47c56e2c92f8a3cba34cf01
Ege Özkan <egeemirozkan24@gmail.com>
2026-04-17 22:41:40 +0300

    feat: create the from-to relationships

--- src/scripts/generateDiagrams.ts
+++ src/scripts/generateDiagrams.ts
@@ -1,4 +1,6 @@
-import { default as milestones } from "../assets/milestones.json";
+import { default as milestones } from "../assets/milestones.json" with { type: "json" };
+import { default as parties } from "../assets/partyUtils.json" with { type: "json" };
+
 type SankeyData = {
   from: string;
   to: string;
@@ -23,13 +25,21 @@ const calculateSankeyDataForPair = (from: Milestone, to: Milestone): SankeyData[
       representatives: toReps
     }
   } = to;
-  const possibleValues = from.snapshot.parties.flatMap(
-    ({ partyName: fromPartyName }) => to.snapshot.parties.map(
-      ({ partyName: toPartyName }) => ({
+  const possibleValues = fromParties.flatMap(
+    ({ partyName: fromPartyName }) => [
+      ...toParties.map(
+        ({ partyName: toPartyName }) => ({
+          from: `${fromPartyName}_${fromSlug}`,
+          to: `${toPartyName}_${toSlug}`,
+          value: 0
+        })),
+      {
         from: `${fromPartyName}_${fromSlug}`,
-        to: `${toPartyName}_${toSlug}`,
+        to: 'vacant',
         value: 0
-      })));
+      }
+    ]);
+
   // Now for each party, find the representatives that moves around
   const sankeyData = fromReps.reduce((track, { name: representativeName, party: partyName}) => {
     // Check if the rep changed parties.
@@ -41,17 +51,25 @@ const calculateSankeyDataForPair = (from: Milestone, to: Milestone): SankeyData[
       // Rep stayed in their own party, marked as found as well.
       // @ts-ignore: every once in a while, some dynamic typing is fine, actually.
       maybeSameRep.found = true;
-      const changedRecord = possibleValues.find(({ from, to }) => from.startsWith(`${partyName}_`) && to.startsWith(`${partyName}_`));
+      const changedRecord = track.find(({ from, to }) => from.startsWith(`${partyName}_`) && to.startsWith(`${partyName}_`));
       changedRecord!.value++;
-      return possibleValues;
+      return track;
     }
     // Now, if it IS changed, we need to find the representatives new "home"
     // @ts-ignore: likewise.
     const newRepRecord = toReps.find(({ name, party, found = false }) => !found && name === representativeName && party !== partyName);
-    return possibleValues;
+    if (!newRepRecord) {
+      const changedRecord = track.find(({ from, to }) => from.startsWith(`${partyName}_`) && to === 'vacant');
+      changedRecord!.value++;
+      return track;
+    }
+    const changedRecord = track.find(({ from, to }) => from.startsWith(`${partyName}_`) && to.startsWith(`${newRepRecord.party}_`)); 
+    changedRecord!.value++;
+    // @ts-ignore: likewise.
+    newRepRecord.found = true;
+    return track;
   }, possibleValues);
   return sankeyData;
-  
 }
 
 const calculateData = (): SankeyData[] => {
@@ -60,12 +78,36 @@ const calculateData = (): SankeyData[] => {
   // have [[1, 2], [2, 3], [3, 4]]
   // ie: sliding window.
   const entries = Object.entries(milestoneOfTerm);
-  const pairs = new Array(entries.length - 1).map((i) => [
+  const pairs = [...new Array(entries.length - 1).keys()].map((_, i) => [
     entries[i],
     entries[i + 1],
   ]);
-  return pairs.flatMap(([[_, from], [__, to]]) => calculateSankeyDataForPair(from, to));
+  return pairs.flatMap(([[_, from], [__, to]]) => calculateSankeyDataForPair(from, to)).filter(({ value }) => !!value);
 };
+
+const shortenPartyName = (partyName: string): string => {
+  const [canonicalLongName, milestone] = partyName.split('_');
+  const partyRecord = parties.parties.find(({ canonicalLongName: longName }) => longName === canonicalLongName);
+  if (partyRecord) {
+    return `${partyRecord.canonicalShortName}_${milestone}`
+  }
+  return partyName;
+}
+
+const noUnicode = (text: string) => text.split('').map(ch => ({
+  'ü': 'u',
+  'Ü': 'U',
+  'İ': 'I',
+  'ı': 'i',
+  'ö': 'o',
+  'Ö': 'O',
+  'ş': 'ş',
+  'Ş': 'S',
+  "ğ": "g",
+  "Ğ": "G",
+  "ç": "c",
+  "Ç": "C"
+}[ch] ?? ch)).join('')
 
 /**
  * Convert JS sankey data to mermaid markup for sankey
@@ -74,9 +116,12 @@ const calculateData = (): SankeyData[] => {
  * @returns Mermaid acceptable markup
  */
 const convertDataToMarkup = (data: SankeyData[]): string =>
-  data.map((o) => Object.values(o).join(", ")).join("\n");
+  data.map(({ from, to, value}) => noUnicode(`"${shortenPartyName(from)}","${shortenPartyName(to)}",${value}`)).join("\n");
 
 const generateDiagrams = () => {
   const data: SankeyData[] = calculateData();
   const mermaidMarkup: string = convertDataToMarkup(data);
+  console.log(mermaidMarkup)
 };
+
+generateDiagrams();
