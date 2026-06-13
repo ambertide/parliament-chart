4c995f7e645c132ed0d4c12e02ddf5c961da6428
Ege Özkan <egeemirozkan24@gmail.com>
2026-06-13 17:44:06 +0300

    refactor: properly set values

--- src/components/ParliamentFigure/ParliamentFigure.tsx
+++ src/components/ParliamentFigure/ParliamentFigure.tsx
@@ -4,34 +4,11 @@ import { PartyLegend } from "../PartyLegend";
 import { BlankMapSVG } from "@/assets/images/BlankMapSVG";
 import { useDiagramMode } from "./useDiagramMode";
 import { Representative } from "@/types";
+import { RepresentativeSeat } from "./RepresentativeSeat";
 
 type ParliamentFigureProps = { representatives: Representative[] }& 
   ComponentProps<typeof PartyLegend>
 ;
-
-const ANIMATION_DURATION = 500;
-
-
-const determineAnimationProps = (representative: Representative, diagramMode: 'map' | 'chart') => {
-  const { x: xFrom, y: yFrom } = diagramMode === 'map' ? representative.mapLocation : representative.location;
-  const { x: xTo, y: yTo } = diagramMode === 'chart' ? representative.mapLocation : representative.location;
-  const rFrom = diagramMode === 'chart' ? 3.5 : 2.5;
-  const rTo = diagramMode === 'chart' ? 2.5 : 3.5;
-  return {
-    cx: {
-      from: xFrom,
-      to: xTo
-    },
-    cy: {
-      from: yFrom,
-      to: yTo
-    },
-    r: {
-      from: rFrom,
-      to: rTo
-    }
-  };
-};
 
 export const ParliamentFigure: FC<ParliamentFigureProps> = ({
   representatives,
@@ -65,38 +42,13 @@ export const ParliamentFigure: FC<ParliamentFigureProps> = ({
           {representatives.map((
             representative,
             index
-          ) => {
-            const animationValues = determineAnimationProps(representative, diagramMode);
-            return (
-              <circle
-                key={index}
-                fill={representative.party.partyColor}
-                {
-                  ...Object.fromEntries(Object.entries(animationValues)
-                    // Set the initial values to that of the [attribute].from
-                    .map(([attributeName, { from, to: _discardForDefault }]) => ([attributeName, from]))
-                  )
-                }
-              >
-                {
-                  // Then add animations for each attributes from -> to pair.
-                  // First convert to prop objects and then into the
-                  // animate tags
-                  Object.entries(animationValues)
-                    .map(([attributeName, { from, to }]) => ({ attributeName, from, to}))
-                    .map( animProps => <animate
-                      key={`repr-${index}-${animProps.attributeName}-animation`}
-                      className="to-map-animation"
-                      dur={`${ANIMATION_DURATION}ms`}
-                      repeatCount="once"
-                      fill="freeze"
-                      begin="indefinite"
-                      {...animProps}
-                    />)
-                }
-              </circle>
-            );
-          })}
+          ) =>
+            <RepresentativeSeat
+              key={index}
+              representative={representative}
+              diagramMode={diagramMode}
+            />
+          )}
         </svg>
       </div>
       <figcaption>
