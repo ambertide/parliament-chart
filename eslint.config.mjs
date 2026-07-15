5a47215d2a5471ca6653903413be37500e4f2a2e
Ege Özkan <egeemirozkan24@gmail.com>
2026-07-15 12:57:49 +0300

    chore: move eslint to root directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -37,12 +34,13 @@ const eslintConfig = defineConfig([
   }, // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    "**/.next/**",
+    "**/out/**",
+    "**/build/**",
+    "**/next-env.d.ts",
+    "**/tmp/**",
+    "**/dist/**",
   ]),
-  ...storybook.configs["flat/recommended"],
 ]);
 
 export default eslintConfig;
