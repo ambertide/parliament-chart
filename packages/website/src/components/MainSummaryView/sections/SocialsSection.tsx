a68e19ba796dde5f3c1b189d846a9f0842d28076
Ege Özkan <egeemirozkan24@gmail.com>
2026-07-19 01:32:50 +0300

    chore(website): instagram update

--- packages/website/src/components/MainSummaryView/sections/SocialsSection.tsx
+++ packages/website/src/components/MainSummaryView/sections/SocialsSection.tsx
@@ -2,7 +2,8 @@ import { useTranslations } from "next-intl";
 import { ComponentProps, FC } from "react";
 import XIcon from '@/assets/images/icons/xIcon.svg';
 import GithubWhiteIcon from '@/assets/images/icons/githubWhiteIcon.svg';
-import MastadonIcon from '@/assets/images/icons/mastadonIcon.svg';
+import InstagramIcon from '@/assets/images/icons/instagram.svg';
+
 import { SectionWrapper } from "./SectionWrapper";
 
 const SocialButton: FC<{ title: string, icon: React.ReactNode } & ComponentProps<'a'>> = ({
@@ -21,7 +22,7 @@ const SocialButton: FC<{ title: string, icon: React.ReactNode } & ComponentProps
 
 const SocialIcons = {
   "x": <XIcon height={20}/>,
-  "mastadon": <MastadonIcon height={20} />,
+  "instagram": <InstagramIcon height={20} />,
   "github": <GithubWhiteIcon height={20} />,
   "rss": <span className="text-xl flex place-items-center text-[#F26522] font-[Material_Symbols_Outlined] h-5">&#xe0e5;</span>,
   "email": <span className="text-xl flex place-items-center text-foreground font-[Material_Symbols_Outlined] h-5">&#xf187;</span>
@@ -34,10 +35,9 @@ export const SocialsSection = ({ className }: { className: string }) => {
     title={t('Follow on Socials')}
   >
     <div className="grow flex flex-col gap-1 items-stretch">
+      <SocialButton title={t('Follow on Instagram')} icon={SocialIcons.instagram} href="" />
       <SocialButton title={t('Follow on X')} icon={SocialIcons.x} href="https://x.com/parlichartcom" />
       <SocialButton title={t('Browse on GitHub')} icon={SocialIcons.github} href="https://github.com/ambertide/parlichart" />
-      <SocialButton title={t('Follow on Mastadon')} icon={SocialIcons.mastadon} href="" />
-      <SocialButton title={t('Subscribe to RSS')} icon={SocialIcons.rss} href="" />
       <SocialButton title={t('Contact us On Email')} icon={SocialIcons.email} href="mailto:contact@mail.parlichart.com" />
     </div>
   </SectionWrapper>;
