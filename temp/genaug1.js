ce556b56bd41a0762d2f87375330eb5c47b5fa19
Ege Özkan <egeemirozkan24@gmail.com>
2026-08-01 22:40:59 +0300

    feat(events): add passed events
    Affected-Term: 27

--- temp/genaug1.js
+++ temp/genaug1.js
@@ -1,48 +1,29 @@
 const events = [
-  ['Hayati Arkaz', '2018-08-13', 'https://web.archive.org/web/20200718114245/https://www.sozcu.com.tr/2018/gundem/hayati-arkaz-iyi-partiden-istifa-etti-2572599/', '2018-08-14', 'Milliyetçi Hareket Partisi', 'https://www.ntv.com.tr/turkiye/iyi-partiden-istifa-eden-hayati-arkaz-mhpye-katildi,WISAYTpLPkGpn4u5qEE8hA'],
-  ['Tamer Akkal', '2019-01-28', 'https://www.haberturk.com/iyi-parti-manisa-milletvekili-tamer-akkal-partisinden-istifa-etti-2304640', '2019-02-05', 'Adalet ve Kalkınma Partisi', 'https://www.aa.com.tr/tr/gunun-basliklari/iyi-partiden-istifa-eden-tamer-akkal-ak-partiye-gecti/1383818'],
-  ['Mustafa Yeneroğlu', '2019-10-30', 'https://tr.euronews.com/2019/10/30/akparti-milletvekili-yeneroglu-istifa-etti-partide-bircok-arkadas-benden-farkli-dusunmuyor', '2020-03-09', 'Demokrasi ve Atılım Partisi', 'https://bianet.org/haber/babacan-in-partisi-deva-nin-basvurusu-yapildi-221126'],
-  ['Tuba Vural Çokal', '2020-02-13', 'https://www.sozcu.com.tr/2020/gundem/meral-aksenerden-istifa-aciklamasi-5625869/', '2020-03-11', 'Adalet ve Kalkınma Partisi', 'https://www.internethaber.com/iyi-partiden-istifa-eden-tuba-vural-cokal-ak-partiye-geciyor-2087758h.htm/'],
-  ['İsmail Ok', '2020-02-17', 'https://web.archive.org/web/20200719091131/https://www.hurriyet.com.tr/gundem/son-dakika-haberler-iyi-parti-balikesir-milletvekili-ismail-ok-partideki-gorevlerinden-istifa-etti-41448956', '2022-04-20', 'Adalet ve Kalkınma Partisi', 'https://web.archive.org/web/20220420100251/https://www.haber7.com/siyaset/haber/3215130-balikesir-milletvekili-ismail-ok-ak-partiye-katildi'],
-  ['Cihangir İslam', '2020-03-04', 'https://www.sozcu.com.tr/2020/gundem/milletvekili-cihangir-islam-saadet-partisinden-istifa-etti-5661080/', '2021-03-09', 'Cumhuriyet Halk Partisi', 'https://web.archive.org/web/20210309182204/https://www.sozcu.com.tr/2021/gundem/cihangir-islam-chpye-katildi-6304472/'],
-  ['Ahmet Şık', '2020-05-04', 'https://www.sozcu.com.tr/2021/gundem/ahmet-sik-turkiye-isci-partisine-katildi-6382941/', '2021-04-19', 'Türkiye İşçi Partisi', 'https://www.sozcu.com.tr/2021/gundem/ahmet-sik-turkiye-isci-partisine-katildi-6382941/'],
-  ['İsmail Koncuk', '2020-11-16', 'https://t24.com.tr/haber/iyi-parti-adana-milletvekili-ismail-koncuk-partisinden-istifa-etti,915110', '2021-08-26', 'Zafer Partisi', 'https://web.archive.org/web/20210826104823/https://www.haberturk.com/zafer-partisi-kuruculari-kimler-iste-zfer-partisi-kurucular-kurulu-uyeleri-tam-liste-3172362'],
-  ['İsmail Koncuk', '2022-04-07', 'https://web.archive.org/web/20220407121806/https://www.cumhuriyet.com.tr/siyaset/adana-milletvekili-ismail-koncuk-zafer-partisinden-istifa-etti-bu-yonetim-anlayisi-ile-maalesef-1923521', '2022-05-25', 'İYİ Parti', 'https://www.cumhuriyet.com.tr/turkiye/ismail-koncuk-yeniden-iyi-parti-saflarina-katiliyor-1939553'],
-  ['Hüseyin Avni Aksoy', '2021-01-28', 'https://www.sozcu.com.tr/2021/gundem/son-dakika-uc-milletvekili-chpden-istifa-etti-6234599/', '2021-05-17', 'Memleket Partisi', 'https://www.bbc.com/turkce/haberler-turkiye-57143930'],
-  ['Özcan Özel', '2021-01-28', 'https://www.sozcu.com.tr/2021/gundem/son-dakika-uc-milletvekili-chpden-istifa-etti-6234599/', '2021-05-17', 'Memleket Partisi', 'https://www.bbc.com/turkce/haberler-turkiye-57143930'],
-  ['Mehmet Ali Çelebi', '2021-01-28', 'https://www.sozcu.com.tr/2021/gundem/son-dakika-uc-milletvekili-chpden-istifa-etti-6234599/', '2021-05-17', 'Memleket Partisi', 'https://www.bbc.com/turkce/haberler-turkiye-57143930'],
-  ['Mehmet Ali Çelebi', '2022-02-25', 'https://web.archive.org/web/20220225064907/https://www.birgun.net/haber/mehmet-ali-celebi-partisini-elestirerek-istifa-etti-378457', '2022-02-25', 'Adalet ve Kalkınma Partisi', 'https://web.archive.org/web/20221011084302/https://www.haberturk.com/mehmet-ali-celebi-ak-parti-ye-gecti-3528227'],
+  ['Erdin Bircan', '2018-11-18', 'http://www.hurriyet.com.tr/gundem/son-dakika-chpli-milletvekili-erdin-bircan-hayatini-kaybetti-40966801'],
+  ['Kazım Arslan', '2019-06-07', 'https://web.archive.org/web/20200718114409/https://www.sozcu.com.tr/2019/gundem/son-dakika-chp-denizli-milletvekili-kazim-arslan-hayatini-kaybetti-5063320/'],
+  ['Markar Esayan', '2020-10-16', 'https://www.haberturk.com/son-dakika-ak-partili-milletvekili-markar-esayan-hayatini-kaybetti-2838151'],
+  ['İsmet Uçma', '2021-10-11', 'https://web.archive.org/web/20211011140434/https://www.cnnturk.com/turkiye/son-dakika-ak-parti-milletvekili-ismet-ucma-hayatini-kaybetti'],
+  ['İmran Kılıç', '2021-10-19', 'https://web.archive.org/web/20211118183909/https://www.sozcu.com.tr/2021/gundem/akp-milletvekili-imran-kilic-hayatini-kaybetti-6777028/'],
+  ['Yakup Taş', '2023-02-07', 'https://www.cumhuriyet.com.tr/siyaset/son-dakika-akp-milletvekili-yakup-tas-yasamini-yitirdi-2049186'],
+  ['Deniz Baykal', '2023-02-11', 'https://www.sozcu.com.tr/2023/gundem/son-dakika-deniz-baykal-hayatini-kaybetti-7588040/']
 ];
 
-const jsons = events.flatMap((([
+const jsons = events.map((([
   name,
-  resignDate,
-  resignSource,
-  switchDate,
-  newParty,
-  switchSource,
-]) => [
+  passedDate,
+  passedSource,
+]) => (
   {
-    "action": "PARTY_CHANGED",
+    "action": "OFFICE_VACATED",
     "actor": name,
-    "date": `${resignDate}T00:00:00.000Z`,
+    "date": `${passedDate}T00:00:00.000Z`,
     "metadata": {
-      "reason": "OTHER"
+      "reason": "PASSED"
     },
-    "source": resignSource,
-    "target": "Bağımsız"
-  },
-  {
-    "action": "PARTY_CHANGED",
-    "actor": name,
-    "date": `${switchDate}T00:00:00.000Z`,
-    "metadata": {
-      "reason": "OTHER"
-    },
-    "source": switchSource,
-    "target": newParty
+    "source": passedSource,
+    "target": "PARLIAMENT"
   }
-]
+)
 ));
 
 console.log(JSON.stringify(jsons));
